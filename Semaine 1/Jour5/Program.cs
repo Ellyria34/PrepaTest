d8b221d816c2a5d40ff91906bee93490bcd0fe3b
Ellyria34 <leon.sarah24@gmail.com>
2025-12-20 22:56:02 +0100

    Test Final J5

--- Semaine 1/Jour5/Program.cs
+++ Semaine 1/Jour5/Program.cs
@@ -7,6 +7,7 @@ List<int> listB = [3, 4, 5];
 
 if(listA != null && listB != null && listA.Count > 0 && listB.Count > 0)
 {
+    //Exercice 1
     Console.WriteLine("Exercice 1 - Écrire une méthode qui compte combien de nombres de listA sont présents dans listB");
     int countCommon = CountCommon(listA, listB);
     if(countCommon > 0)
@@ -17,6 +18,7 @@ if(listA != null && listB != null && listA.Count > 0 && listB.Count > 0)
     {
         Console.WriteLine($"La liste A et la liste B n'ont aucun nombre en commun.");
     }
+    //Exercice 2
     Console.WriteLine("Exercice 2 - Est ce que tous les nombres de la liste A sont présent dans la liste B?");
     bool allInSecondList = AllInSecondList(listA, listB);
     if(allInSecondList)
@@ -27,6 +29,11 @@ if(listA != null && listB != null && listA.Count > 0 && listB.Count > 0)
     {
         Console.WriteLine("Non, tous les nombres de la liste A ne se trouve pas dans la liste B.");
     }
+
+    //Test Final
+    Console.WriteLine("Test final - Retourne la somme des nombres STRICTEMENT positifs présents dans les DEUX listes.");
+    int sumCommonPositive = SumCommonPositiv(listA, listB);
+    Console.WriteLine($"La somme des nombres strictement positifs et présents dans les deux listes est égale à : {sumCommonPositive}.");
 }
 else
 {
@@ -72,4 +79,25 @@ static bool AllInSecondList(List<int> listA, List<int> listB)
         }
     }
     return true;
+}
+
+static int SumCommonPositiv(List<int> listA, List<int> listB)
+{
+    int sum = 0;
+
+    foreach(int nA in listA)
+    {
+        if(nA > 0)
+        {
+            foreach (int nB in listB)
+            {
+                if(nA == nB)
+                {
+                    sum = sum + nA;
+                    break;
+                }
+            }
+        }
+    }
+    return sum;
 }
