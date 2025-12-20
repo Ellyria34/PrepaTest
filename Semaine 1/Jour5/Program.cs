e3c05ab8af9c81677a9f97a8470690ad1f8dfaee
Ellyria34 <leon.sarah24@gmail.com>
2025-12-20 22:25:34 +0100

    Exercice 2 J5

--- Semaine 1/Jour5/Program.cs
+++ Semaine 1/Jour5/Program.cs
@@ -1,11 +1,13 @@
-﻿Console.WriteLine("Jour 5");
+﻿using System.ComponentModel;
+
+Console.WriteLine("Jour 5");
 
 List<int> listA = [1, 2, 3, 4];
 List<int> listB = [3, 4, 5];
 
-Console.WriteLine("Exercice 1 - Écrire une méthode qui compte combien de nombres de listA sont présents dans listB");
 if(listA != null && listB != null && listA.Count > 0 && listB.Count > 0)
 {
+    Console.WriteLine("Exercice 1 - Écrire une méthode qui compte combien de nombres de listA sont présents dans listB");
     int countCommon = CountCommon(listA, listB);
     if(countCommon > 0)
     {
@@ -14,6 +16,16 @@ if(listA != null && listB != null && listA.Count > 0 && listB.Count > 0)
     else
     {
         Console.WriteLine($"La liste A et la liste B n'ont aucun nombre en commun.");
+    }
+    Console.WriteLine("Exercice 2 - Est ce que tous les nombres de la liste A sont présent dans la liste B?");
+    bool allInSecondList = AllInSecondList(listA, listB);
+    if(allInSecondList)
+    {
+        Console.WriteLine("Oui tous les nombres de la liste A se trouve dans la liste B.");
+    }
+    else
+    {
+        Console.WriteLine("Non, tous les nombres de la liste A ne se trouve pas dans la liste B.");
     }
 }
 else
@@ -38,4 +50,26 @@ static int CountCommon(List<int> listA, List<int> listB)
         }
     }
     return result;
+}
+
+static bool AllInSecondList(List<int> listA, List<int> listB)
+{
+    foreach(int nA in listA)
+    {
+        bool found = false;
+
+        foreach(int nB in listB)
+        {
+            if(nA == nB)
+            {
+                found = true;
+                break;
+            }
+        }
+        if(!found)
+        {
+            return false;
+        }
+    }
+    return true;
 }
