c9a43235cddf5a0eeecdc0a48dea05e088208921
Ellyria34 <leon.sarah24@gmail.com>
2025-12-22 23:08:58 +0100

    Partie C - Exercice Test Final J

--- Semaine 1/Jour6/Program.cs
+++ Semaine 1/Jour6/Program.cs
@@ -4,7 +4,9 @@
 using System.ComponentModel;
 
 Console.WriteLine("Partie A - Test Révision : Écrire une méthode qui retourne le plus petit nombre STRICTEMENT positif commun aux deux listes.");
-List<int> list = [5, -2, 0, 3, 0, -1]; 
+// List<int> list = [5, -2, 0, 3, 0, -1]; 
+List<int> list = [1, 2, 0]; 
+
 List<int> listA = [1, 3, 5];
 List<int> listB = [2, 3, 4, 5];
 // List<int> listA = [-1, -2];
@@ -19,14 +21,22 @@ else
     Console.WriteLine("Une des deux listes est vide");
 }
 
-//Partie B : Nouveau pattern
-Console.WriteLine("Partie B - Nouveau pattern : Écrire une méthode qui compte séparément :le nombre de nombres strictement positifs, le nombre de nombres strictement négatifs et le nombre de zéros");
 if(list != null && list.Count > 0)
 {
+    //Partie B : Nouveau pattern
+    Console.WriteLine("Partie B - Nouveau pattern : Écrire une méthode qui compte séparément :le nombre de nombres strictement positifs, le nombre de nombres strictement négatifs et le nombre de zéros");  
     int positives, negatives, zeros;
     CountBySign(list, out positives, out negatives, out zeros);
-
     Console.WriteLine($"Il y a {positives} nombre positifs, {negatives} nombre négatifs et {zeros} zero dans la list.");
+
+    //Partir C : Test final J6
+    Console.WriteLine("Partie C - Test : Écrire une méthode qui retourne true si la liste respecte TOUTES les règles suivantes : la liste contient au moins un nombre positif, la liste ne contient aucun nombre strictement négatif et la sommes des nombres est strictement supérieur à 0.");  
+    bool isValisdateSequence = IsValidSequence(list);
+    if(isValisdateSequence)
+    {
+        Console.WriteLine($"La liste respecte les conditions");
+    }
+    else Console.WriteLine($"La liste ne respecte pas les conditions");
 }
 else
 {
@@ -75,4 +85,32 @@ static void CountBySign(List<int> list, out int positives, out int negatives, ou
             zeros ++;
         }
     }
+}
+
+static bool IsValidSequence(List<int> list)
+{
+    bool onePositif = false;
+
+    int sum = 0;
+    foreach (int n in list)
+    {
+        if(n < 0)
+        {
+            return false;
+        }
+        else
+        {
+            if(n > 0)
+            {
+                onePositif = true;
+            }
+            sum = sum + n;
+        }
+    }
+
+    if(sum <= 0 || !onePositif)
+    {
+        return false;
+    }
+    else return true;
 }
