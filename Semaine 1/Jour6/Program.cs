b8d423e8cd85dda007ff2d22bb2c81f1bb31fa99
Ellyria34 <leon.sarah24@gmail.com>
2025-12-22 17:27:21 +0100

    Partie B - exercice nouveau pattern

--- Semaine 1/Jour6/Program.cs
+++ Semaine 1/Jour6/Program.cs
@@ -4,20 +4,36 @@
 using System.ComponentModel;
 
 Console.WriteLine("Partie A - Test Révision : Écrire une méthode qui retourne le plus petit nombre STRICTEMENT positif commun aux deux listes.");
+List<int> list = [5, -2, 0, 3, 0, -1]; 
 List<int> listA = [1, 3, 5];
 List<int> listB = [2, 3, 4, 5];
 // List<int> listA = [-1, -2];
 // List<int> listB = [1, 2];
-if(listA != null && listB != null && listA.Count() > 0 && listB.Count() > 0)
+if(listA != null && listB != null && listA.Count > 0 && listB.Count > 0)
 {
     int minCommonPositive = MinCommonPositive(listA, listB);
-    Console.WriteLine($"{minCommonPositive} est plus petit nombre positif contenu dans la listA et listB.");
+    Console.WriteLine($"{minCommonPositive} est plus petit nombre positif contenu dans la listA et listB.");    
 }
 else
 {
     Console.WriteLine("Une des deux listes est vide");
 }
 
+//Partie B : Nouveau pattern
+Console.WriteLine("Partie B - Nouveau pattern : Écrire une méthode qui compte séparément :le nombre de nombres strictement positifs, le nombre de nombres strictement négatifs et le nombre de zéros");
+if(list != null && list.Count > 0)
+{
+    int positives, negatives, zeros;
+    CountBySign(list, out positives, out negatives, out zeros);
+
+    Console.WriteLine($"Il y a {positives} nombre positifs, {negatives} nombre négatifs et {zeros} zero dans la list.");
+}
+else
+{
+    Console.WriteLine("Une des deux listes est vide");
+}
+
+//Methodes
 static int MinCommonPositive(List<int> listA, List<int> listB)
 {
     int minCommonPositive = -1;
@@ -36,4 +52,27 @@ static int MinCommonPositive(List<int> listA, List<int> listB)
         }
     }
     return minCommonPositive;
+}
+
+static void CountBySign(List<int> list, out int positives, out int negatives, out int zeros)
+{
+    positives = 0;
+    negatives = 0;
+    zeros = 0;
+
+    foreach(int n in list)
+    {
+        if (n < 0)
+        {
+            negatives ++;
+        }
+        else if (n > 0)
+        {
+            positives ++;
+        }
+        else
+        {
+            zeros ++;
+        }
+    }
 }
