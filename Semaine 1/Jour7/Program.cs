5ab302a73f179e2b9864b371926354df272e4448
Ellyria34 <leon.sarah24@gmail.com>
2025-12-25 16:59:48 +0100

    Jour1 Semain2

---
+++ Semaine 1/Jour7/Program.cs
@@ -0,0 +1,75 @@
+﻿Console.WriteLine("Jour 7");
+
+//List<int> numbers = [5, 4, 3, 1];
+List<int> numbers = [5, 10];
+
+//Exercice Révision
+Console.WriteLine("Partie A - Test de révision");
+Console.WriteLine("Montrer si la liste est strictement décroissantes.");
+bool isStrictlyDecreasing = IsStrictlyDecreasing(numbers);
+
+if(isStrictlyDecreasing)
+{
+    Console.WriteLine("Oui la liste est strictement décroissantes.");    
+}
+else
+{
+    Console.WriteLine("Non la liste est strictement décroissantes.");
+}
+
+
+//Exercice nouveau pattern
+Console.WriteLine("Partie B - Nouveau pattern");
+Console.WriteLine("Montrer si la moyenne des nombres STRICTEMENT positifs de la liste est STRICTEMENT supérieure à 10.");
+bool averagePositiveGreaterThanTen = AveragePositiveGreaterThanTen(numbers);
+if(averagePositiveGreaterThanTen)
+{
+    Console.WriteLine("Oui la moyenne des nombres STRICTEMENT positifs de la liste est STRICTEMENT supérieure à 10.");    
+}
+else
+{
+    Console.WriteLine("Non la moyenne des nombres STRICTEMENT positifs de la liste est STRICTEMENT supérieure à 10.");
+}
+
+
+//Methods
+static bool IsStrictlyDecreasing(List<int> numbers)
+{
+    for(int i = 0; i < numbers.Count - 1; i++)
+    {
+        if (numbers[i] <= numbers[i + 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+static bool AveragePositiveGreaterThanTen(List<int> numbers)
+{
+    int countNumber = 0;
+    int sum = 0;
+    int average = 0;
+
+    foreach(int n in numbers)
+    {
+        if(n > 0)
+        {
+            sum = sum + n;
+            countNumber ++;
+        }
+    }
+
+    if(countNumber!=0)
+    {
+        average = sum/countNumber;
+    }
+    
+    if(average > 10)
+    {
+        return true;
+    }
+    return false;
+}
+
+
---
+++ Semaine 2/Jour1Semaine2/Program.cs
@@ -0,0 +1,33 @@
+﻿//Jour1 Semaine 2
+using System.ComponentModel;
+
+//List<int> numbers = [1, 2, 2, 3];
+//List<int> numbers = [1, 2, 3, 3];
+//List<int> numbers = [1, 2, 3];
+//List<int> numbers = [5];
+List<int> numbers = [];
+
+Console.WriteLine("Test révision - Est ce que la liste contient deux nombres consécutifs identiques ?");
+bool hasConsecutiveDuplicates = HasConsecutiveDuplicates(numbers);
+if (hasConsecutiveDuplicates)
+{
+    Console.WriteLine("Oui, il y a au moins deux nombres consécutifs.");    
+}
+else
+{
+    Console.WriteLine("Non, il n'y a pas deux nombres consécutifs.");    
+}
+
+//Methods
+static bool HasConsecutiveDuplicates(List<int> numbers)
+{
+    for (int i = 0; i < numbers.Count-1; i++)
+    {
+        if(numbers[i] == numbers[i+1])
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
