15fcca35ed21f27723ca6bc68a3341a598806abc
Ellyria34 <leon.sarah24@gmail.com>
2026-01-01 05:58:35 +0100

    Jour7 Semaine2 Partie B

--- Semaine 2/Jour7Semaine2/Program.cs
+++ Semaine 2/Jour7Semaine2/Program.cs
@@ -1,21 +1,35 @@
 ﻿// Jour 7 Semaine 2
+using System.Runtime.InteropServices;
+
 Console.WriteLine("Jour 7 Semain 2");
 
 string sentence = "La phrase contient au moins un mot de plus de 5 caractères et ne commence et ne se termine pas par un espace.";
 
-Console.WriteLine("Ecrire une méthode pour vérifier que la phrase conyient au moins un mot de plus de 5 caractères et qu'elle ne commence et ne se termine pas par un espace.");
-bool isValide = IsValide(sentence); 
-if(isValide)
+Console.WriteLine("Partie A - Ecrire une méthode pour vérifier que la phrase contient au moins un mot de plus de 5 caractères et qu'elle ne commence et ne se termine pas par un espace.");
+bool isValidForA = IsValidForA(sentence); 
+if(isValidForA)
 {    
-    Console.WriteLine("Oui, la phrase est valise.");
+    Console.WriteLine("Oui, la phrase est valide.");
 }
 else 
 {
-    Console.WriteLine("Non, la phrase n'est pas valise.");
+    Console.WriteLine("Non, la phrase n'est pas valide.");
+}
+
+Console.WriteLine("Partie B - Ecrire une méthode pour vérifier que la phrase contient au moins 3 mots, aucun moy vide \"\", aucun mot ne commencant ou ne se terminant par un espace, avec au moins un mot ayant plus de 5 caractères et aucun mot contenant 2 lettres indentiques consécutives.");
+List<string> words = sentence.Split(" ");
+bool isValidForB = IsValidForB(words); 
+if(isValidForB)
+{    
+    Console.WriteLine("Oui, la phrase est valide.");
+}
+else 
+{
+    Console.WriteLine("Non, la phrase n'est pas valide.");
 }
 
 //Methods
-static bool IsValide(string sentence)
+static bool IsValidForA(string sentence)
 {
     int currentWordLength = 0;
     if(string.IsNullOrEmpty(sentence))
@@ -44,4 +58,36 @@ static bool IsValide(string sentence)
         }
     }
     return false;
+}
+
+static bool IsValidForB(List<string> words)
+{
+    int wordsCount = words.Count;
+    bool isLongerThan5Char = false;
+    foreach(string word in words)
+    {
+        if(word == "")
+        {
+            return false;
+        }
+        if(word.Length>5)
+        {
+            isLongerThan5Char = true;
+        }
+        for(int i = 0; i < word.Length-1; i++)
+        {
+            if(word[i] == word[i+1])
+            {
+                return false;
+            }
+        }
     }
+    if(wordsCount >= 3 && isLongerThan5Char)
+    {
+        return true;
+    }
+    else 
+    {
+        return false;
+    }
+}
